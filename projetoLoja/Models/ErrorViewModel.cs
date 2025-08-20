d97f55a0505f10bbfee0119a882a1e99240f09a6
Joorb <joorb411@gmail.com>
2025-08-20 07:46:36 -0300

    Criando as controllers e as models

---
+++ projetoLoja/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projetoLoja.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
